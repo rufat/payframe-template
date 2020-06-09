9e2d4358d1788da16d568c81a377758806ead1a2
Rufat <rufatmdl@gmail.com>
2020-06-10 02:35:44 +0400

    Transfer amount step added. Minor fixes.

--- src/containers/Wizard/Amount/index.js
+++ src/containers/Wizard/Amount/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Amount() {
     return (
-        <div>
-
+        <div className={'amount-step'}>
+            <div className={'amount c-input-tile'}>
+                <span>Tutar</span>
+                <input type={'number'} min={1} placeholder={'Tutar'}/>
+            </div>
+            <span>Limit : 100TL - 25.000TL / İşlem Ücreti : %2.5 (22.25 TRY)</span>
         </div>
     )
 }
